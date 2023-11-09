91b50b07999d024eb46921be107361ac5269e329
Camilo Tacue <cetacue@misena.edu.co>
2023-11-09 17:55:06 -0500

    SharedPreferences

--- app/src/main/java/com/example/pruebas/MainActivity.kt
+++ app/src/main/java/com/example/pruebas/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.example.pruebas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.pruebas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnGoLogin.setOnClickListener {
+            val intent = Intent(this, Home_Activity::class.java)
+            startActivity(intent)
+        }
     }
 }
