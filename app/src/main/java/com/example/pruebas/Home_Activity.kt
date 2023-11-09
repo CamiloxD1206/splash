bc9c1d0c9a932e78b720480540d7e8b1be41eed1
Camilo Tacue <cetacue@misena.edu.co>
2023-11-09 18:28:34 -0500

    SharedPreferences3

--- app/src/main/java/com/example/pruebas/Home_Activity.kt
+++ app/src/main/java/com/example/pruebas/Home_Activity.kt
@@ -1,11 +1,22 @@
 package com.example.pruebas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pruebas.databinding.ActivityHomeBinding
+import com.example.pruebas.databinding.ActivityMainBinding
 
 class Home_Activity : AppCompatActivity() {
+    lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding= ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnGoLogout.setOnClickListener{
+            val intent=Intent(this,ActivityMainBinding::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/pruebas/MainActivity.kt
+++ app/src/main/java/com/example/pruebas/MainActivity.kt
@@ -16,6 +16,13 @@ class MainActivity : AppCompatActivity() {
         binding.btnGoLogin.setOnClickListener {
             val intent = Intent(this, Home_Activity::class.java)
             startActivity(intent)
+
+            finish()
+        }
+        binding.btnGoRegister.setOnClickListener {
+            val intent = Intent(this, Register_Activity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
